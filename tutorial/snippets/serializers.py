576538a653ce86e782ad300648d0bfbb4b5f255e
Leon-Mac <leonardoli525@gmail.com>
2016-10-31 18:52:55 -0400

    .DS_Store banished;The MTV of Django is accomplished,the model of User is added;MySQL is set

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Users,Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('id', 'account', 'email', 'age', 'language')
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,8 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
+	url(r'^$',views.index, name="index"),
+    url(r'^snippets/$', views.snippet_list,name='snippets-instance'),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+    url(r'^users/$', views.user_list,name='user-instance'),
+    url(r'^users/(?P<pk>[0-9]+)$', views.user_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
