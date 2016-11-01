3f55a244c52fc3bd4a9378fa4c15b37fd2dedee9
Aaron <11837801d@connect.polyu.hk>
2016-10-31 20:49:49 -0400

    set on the aws

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
